c1b1515b404df422774233271ae760aa40ea9fda
Zachary Mactavish <zmactavish@Zacharys-MacBook-Pro.local>
2026-01-12 14:47:00 -0500

    Add vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // relative path so assets load correctly on any domain
-});
+  base: '/ZackMacTavishArt/'
+})
